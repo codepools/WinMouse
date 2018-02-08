c01364122b8ddf4edbc11b5631e46a59aff8eb22
DESKTOP-ETOV22Q\one <onebravex@gmail.com>
2018-02-08 23:23:17 +0800

    可以用的版本。

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿using System.Drawing;
+
+namespace WinMouse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private Icon IcoRight= new Icon("Direction Right.ico");
+        private Icon IcoLeft = new Icon("Direction Left.ico");
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.退出ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(176, 56);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(175, 24);
+            this.退出ToolStripMenuItem.Text = "退出";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = " ";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(52, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(207, 34);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(189, 173);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 236);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinMouse
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("user32.dll")]
+        private extern static bool SwapMouseButton(bool fSwap);
+       
+
+        [DllImport("user32.dll")]
+        private extern static int GetSystemMetrics(int index);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SetCursor(IntPtr cursorHandle);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr LoadCursorFromFile(string fileName);
+
+        [DllImport("user32.dll")]
+        public static extern bool SetSystemCursor(IntPtr hCursor, int id);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            int flag = GetSystemMetrics(23);//获取当前鼠标设置状态 
+            if (flag == 0)//左手习惯 
+            {
+                LeftMouse();
+            }
+            else//右手习惯 
+            {
+                RightMouse();
+
+            }
+
+
+        }
+        void LeftMouse()
+        {
+            notifyIcon1.Icon = IcoLeft;
+            SwapMouseButton(true);//设置成左手
+         //   LabelText.Text = "左手鼠标";
+
+            string lcur = "aero_arrow_left.cur";
+            if (File.Exists(lcur))
+            {
+                IntPtr cur = LoadCursorFromFile(lcur);//鼠标图标路径  
+                Boolean a = SetSystemCursor(cur, 32512);//32512
+            }
+
+            pictureBox2.Visible = false;
+            pictureBox1.Visible = true;
+
+        }
+        void RightMouse()
+        {
+            notifyIcon1.Icon = IcoRight;
+            SwapMouseButton(false);//设置成右手 aero_arrow.cur
+        //    LabelText.Text = "右手鼠标";
+            string rcur = "aero_arrow.cur";
+            if (File.Exists(rcur))
+            {
+                IntPtr cur = LoadCursorFromFile(rcur);//鼠标图标路径  
+                Boolean a = SetSystemCursor(cur, 32512);//32512
+            }
+
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = true;
+        }
+     
+          
+
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int flag = GetSystemMetrics(23);//获取当前鼠标设置状态 
+            if (flag == 0)//左手习惯 
+            {
+
+                LeftMouse();
+            }
+            else//右手习惯 
+            {
+                RightMouse();
+
+
+            }
+        }
+    }
+}
